a7db351744903d6553db5099bf3395f7399ba577
Damien <damien.frikha@kleegroup.com>
2025-04-06 22:05:32 +0200

    maj focus4 v12 finale

--- sources/front/src/locale/index.ts
+++ sources/front/src/locale/index.ts
@@ -1,17 +1,23 @@
-import {common} from "./common";
-import {router} from "./router";
-import {securite} from "./securite";
+import i18next from "i18next";
+import {initReactI18next} from "react-i18next";
 
 import {i18nCollections} from "@focus4/collections";
-import {initI18n} from "@focus4/core";
+import {baseI18nextConfig} from "@focus4/core";
 import {i18nFormToolbox} from "@focus4/form-toolbox";
 import {i18nLayout} from "@focus4/layout";
 import {i18nStores} from "@focus4/stores";
 
-initI18n("fr", [i18nCollections, i18nFormToolbox, i18nLayout, i18nStores], {
-    fr: {
-        common,
-        securite,
-        router
-    }
+import {common} from "./common";
+import {router} from "./router";
+import {securite} from "./securite";
+
+i18next.use(initReactI18next).init({
+    ...baseI18nextConfig([i18nCollections, i18nFormToolbox, i18nLayout, i18nStores], {
+        fr: {
+            common,
+            securite,
+            router
+        }
+    }),
+    lng: "fr"
 });
